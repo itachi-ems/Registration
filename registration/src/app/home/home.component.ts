ab92fbfd3d43510b20e2b248548516792a801861
itachi-ems <jshahharsh@gmail.com>
2021-05-26 02:26:11 +0530

    Login and registration completed

--- registration/src/app/home/home.component.ts
+++ registration/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {InputData} from '../../Temp/Inputdata';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  data:InputData = new InputData();
+  validData:boolean=false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSubmit(f:any,password:any,emailid:any)
+  {
 
+    
+    this.data.password=password.value;
+    this.data.emailid=emailid.value;
+    
+    console.log(this.data);
+    this.validData=true;
+
+    alert("Login Successful")
+    
+  }
 }
