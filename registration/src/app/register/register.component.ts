ba5715d84d84efc37e4387a8f6ba4a6ced07a8c9
itachi-ems <jshahharsh@gmail.com>
2021-07-05 01:17:29 +0530

    register component completed

--- registration/src/app/register/register.component.ts
+++ registration/src/app/register/register.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {InputData} from '../../Temp/Inputdata';
@@ -23,22 +24,26 @@ export class RegisterComponent implements OnInit {
     "Egypt":"+20"
   };
 
-  data:InputData = new InputData();
+  
   validData:boolean=false;
-  constructor(private router:Router){}
+  constructor(private router:Router,private http:HttpClient){}
 
   ngOnInit(): void {
   }
 
-  onSubmit(f:any,firstname:any,lastname:any,password:any,emailid:any,phoneNo:any)
+  onSubmit(f:any,firstname:any,lastname:any,password:any,emailid:any)
   {
-
-    this.data.firstname=firstname.value;
-    this.data.lastname=lastname.value;
-    this.data.password=password.value;
-    this.data.emailid=emailid.value;
-    this.data.phoneNo=phoneNo.value;
-    console.log(this.data);
+    const data = { 
+      firstname:firstname.value,
+      lastname:lastname.value,
+      emailid:emailid.value,
+      password:password.value
+    }
+    this.http.post("http://july4afternoon-env.eba-rsh2etsk.us-east-2.elasticbeanstalk.com/api/user",data)
+    .subscribe(res=>{
+      console.log(res);
+      
+    })
     this.validData=true;
     
     this.router.navigate(['/']);
